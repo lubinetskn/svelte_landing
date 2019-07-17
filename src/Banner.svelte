7e0d4d35c2f6876f11583d9289d0380d30f431b3
Лубинец Константин Николаевич <konstantin.lubinets@ligastavok.ru>
2019-07-17 18:54:18 +0300

    savea

--- src/Banner.svelte
+++ src/Banner.svelte
@@ -1,4 +1,7 @@
 <style>
+    .banner{
+        background: url('./img/banner-back.jpg')
+    }
     h2 {
         font-size: 32px;
         color: #ffffff;
@@ -13,14 +16,30 @@
         font-weight: 700;
         text-align: center;
     }
+    .btns a{
+        width: 198px;
+        height: 54px;
+        border-radius: 5px;
+        background-color: #2da736;
+        display: block;
+        margin: auto;
+        text-align: center;
+        padding-top: 16px;
+        box-sizing: border-box;
+
+    }
+    .btns a span{
+        color: #fff;
+    }
 </style>
 
 <section class="banner">
     <div class="container">
+        <img src="./img/banner-back.jpg" alt="">
         <h2>Приведите друга на розыгрыш «Вечерняя Лига» и</h2>
         <h3>удвойте свои фрибеты</h3>
         <div class="btns">
-            <a href="#pdf">Подробнее</a>
+            <a href="#pdf"><span>Подробнее</span></a>
         </div>
     </div>
 </section>
