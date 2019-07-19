f603e73221ded6998717495712f73d94ee50e377
Лубинец Константин Николаевич <konstantin.lubinets@ligastavok.ru>
2019-07-19 16:10:50 +0300

    verstka

--- src/Gadjet.svelte
+++ src/Gadjet.svelte
@@ -1,29 +1,68 @@
 <style>
-
+    #gadjet{
+        background-color: #083b30;
+    }
+    h2{
+        font-size: 44px;
+        font-weight: 700;
+    }
+    h3{
+        display: inline-block;
+        font-size: 14px;
+        text-transform: uppercase;
+        border-radius: 4px;
+        color: #003b29;
+        background-color: #d2d700;
+        padding: 2px 4px;
+        font-weight: 700;
+    }
+    .formula{
+        padding: 25px;
+        width: 757px;
+        height: 120px;
+        border-radius: 6px;
+        background-color: #006237;
+    }
+    .ball {
+    display: flex;
+    }
+    .text{
+        font-size: 24px;
+        line-height: 26px;
+        color: #d2d700;
+        font-weight: 700;
+    }
 </style>
-<section class="gadjet">
-    <h2>Розыгрыш гаджетов</h2>
-    <h3>каждый месяц</h3>
-    <img src="" alt="">
-    <div class="yel">Формула расчёта количества купонов:</div>
-    <h4>Наклейки, шт.:</h4>
-    <ul class="ball ball--round">
-        <li>7</li>
-        <li>13</li>
-        <li>18</li>
-        <li>22</li>
-        <li>25</li>
-        <li>27</li>
-        <li>28</li>
-    </ul>
-    <h4>Купоны, шт.:</h4>
-    <ul class="ball ball--squere">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-    </ul>
+<section id="gadjet">
+    <div class="container">
+        <h2>Розыгрыш гаджетов</h2>
+        <h3>каждый месяц</h3>
+        <img src="images/tv.png" alt="">
+        <div class="formula">
+            <div class="yel">Формула расчёта количества купонов:</div>
+            <div class="">
+                <h4>Наклейки, шт.:</h4>
+                <ul class="ball ball--round">
+                    <li>7</li>
+                    <li>13</li>
+                    <li>18</li>
+                    <li>22</li>
+                    <li>25</li>
+                    <li>27</li>
+                    <li>28</li>
+                </ul>
+                <h4>Купоны, шт.:</h4>
+                <ul class="ball ball--squere">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                </ul>
+            </div>
+        </div>
+        <div class="text">Больше наклеек – больше купонов – больше шансов выиграть один из гаджетов!</div>
+    </div>
 </section>
--- src/Pari.svelte
+++ src/Pari.svelte
@@ -1,3 +1,59 @@
-<div class="pari">
-    <h2>Пари, участвующие в розыгрышах</h2>
-</div>
+<style>
+    #pari{
+        background-image: url("images/pari-back.jpg");
+        background-size: cover;
+        text-align: center;
+        padding: 30px 0;
+    }
+    h2{
+        font-size: 44px;
+        line-height: 80px;
+        font-weight: 700;
+    }
+    ul{
+        display: flex
+    }
+    ul li{
+        width: 20%;
+        font-size: 16px;
+        line-height: 24px;
+        padding: 16px;
+    }
+</style>
+<section id="pari">
+    <div class="container">
+        <h2>Пари, участвующие в розыгрышах</h2>
+        <ul>
+            <li>
+                <picture>
+                    <img src="images/pari1.png" alt="">
+                </picture>
+                <div>Вида «одинар» и «экспресс»</div>
+            </li>
+            <li>
+                <picture>
+                    <img src="images/pari2.png" alt="">
+                </picture>
+                <div>Спортивные и не спортивные события</div>
+            </li>
+            <li>
+                <picture>
+                    <img src="images/pari3.png" alt="">
+                </picture>
+                <div>Заключённые с помощью Клубных карт через кассу и/или через терминал</div>
+            </li>
+            <li>
+                <picture>
+                    <img src="images/pari4.png" alt="">
+                </picture>
+                <div>Пари, заключённые с использованием фрибетов, в акции не участвуют</div>
+            </li>
+            <li>
+                <picture>
+                    <img src="images/pari5.png" alt="">
+                </picture>
+                <div>Пари, рассчитанные с коэффициентом 1 (возврат), в акции не участвуют</div>
+            </li>
+        </ul>
+    </div>
+</section>
